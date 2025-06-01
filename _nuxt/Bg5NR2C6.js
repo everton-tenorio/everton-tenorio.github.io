063d088bdc89067459227e9f4c5e9acb1a788fa3
everton-tenorio <everton-tenorio@users.noreply.github.com>
2025-06-01 12:02:38 +0000

    deploy: fc30d34153370f2c7625a16030f5818cebf6cde5

---
+++ _nuxt/Bg5NR2C6.js
@@ -0,0 +1,19 @@
+const e=`---
+title: "Immersive English Learning Through AI-Translated Tweets from the Shell"
+date: 2025-06-01
+slug: ai-language-learning-shell-twitter-cli
+description: ""
+tags: ["x", "twitter", "genAI", "shell script", "cli", "english", "gemini"]
+bannerImage: ""
+---
+
+Using shell script and the \`xurl\` library (a tool for interacting with the X API), I created a CLI to manage tweets directly from the shell. Then, aiming to improve my English skills beyond simple word-for-word reading and writing, I used Google’s generative AI model **Gemini 1.5 Flash** to translate the posts in a natural way, fostering immersive learning.
+
+Integrated via Python, a specific prompt was designed with the original message’s lexical and semantic context in mind. This approach of using AI for language learning aligns with the concept of **Task-Based Learning (TBL)**. TBL emphasizes communication-driven learning through practical and meaningful tasks—in this case, translation and publication induce lexical and grammatical acquisition organically.
+
+Using an **LLM** to translate tweets requires analyzing and reviewing translations, enabling learning through practical examples in real-world situations. It also introduces elements of **gamification**, where interaction through comments and likes serves as motivation, boosting engagement.
+
+One example of how this approach can be applied is for **freelancers** who face communication barriers with international clients. By replacing the X API with platforms like **Slack**, **Teams**, or **LinkedIn**, this methodology could be adapted to translate professional messages, expanding its usefulness in various communicative contexts.
+
+<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7302370497680306177?compact=1" height="399" width="504" frameborder="0" allowfullscreen="" title="Publicação incorporada"></iframe>
+`;export{e as default};
