6e1370de1f690e0ac9d44894ec5ccbb59ad7cdbd
everton-tenorio <everton-tenorio@users.noreply.github.com>
2025-06-01 13:33:20 +0000

    deploy: 303674f3402e685647cd5150584f85ae0f5130ff

---
+++ _nuxt/x3qgJ9V-.js
@@ -0,0 +1,26 @@
+const e=`---
+title: "#DevOpsDiary: An Open Source Project for Practical Learning"
+date: 2025-06-1
+slug: devops-diary-open-source-practical-learning
+description: ""
+tags: [
+  "devops open source project",
+  "learn devops online",
+  "ci cd devops tools",
+  "devsecops best practices",
+  "hands-on devops challenges"
+]
+
+bannerImage: "https://media.licdn.com/dms/image/v2/D4D22AQEhwJTbBzMZHg/feedshare-shrink_1280/B4DZaNB9iyG0Ak-/0/1746122830521?e=1751500800&v=beta&t=j1vaME2pSZNnFvpy6oqtlj48r6h7rzqL4Fazp2TcP60"
+---
+
+Conceived a few weeks ago, **#DevOpsDiary** is an open-source project inspired by existing platforms, but with an exclusive focus on **#DevOps**. This is the first version of the initiative, designed to offer a practical and structured approach to learning in the field.
+
+The goal is to learn through hands-on challenges while applying the most commonly used tools and practices in the DevOps culture, such as **automation**, **CI/CD**, **DevSecOps**, and **collaboration**. Professionals in the field are encouraged to validate existing tasks and suggest new ideas to enrich the project.
+
+
+Links:
+
+- Website: [https://devopsdiary.site](https://devopsdiary.site)
+- Repository: [https://github.com/everton-tenorio/DevOpsDiary](https://github.com/everton-tenorio/DevOpsDiary)
+`;export{e as default};
