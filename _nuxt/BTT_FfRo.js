6d575ede13757e98dfabc29af58b7e5dcaf5c360
everton-tenorio <everton-tenorio@users.noreply.github.com>
2025-06-14 18:42:39 +0000

    deploy: c2771de701875a101da715888834dbfba41a098c

---
+++ _nuxt/BTT_FfRo.js
@@ -0,0 +1,18 @@
+const a=`---
+title: "Portalfut: Mundial de Clubes FIFA 2025"
+date: 2025-06-14
+slug: portalfut-mundial-clubes-2025
+description: ""
+tags: ["futebol", "mundial de clubes 2025", "fifa", "portalfut script"]
+bannerImage: "https://i.ibb.co/YF7DDhZx/Screenshot-2025-05-23-at-23-03-58-pfb-png-imagem-PNG-1024-1536-pixels.png"
+---
+
+A Copa do Mundo de Clubes começa hoje, e para quem quer acompanhar sem perder tempo buscando onde os jogos serão transmitidos, notícias ou estatísticas: acesse [portalfut.com](https://portalfut.com).
+
+---
+
+O PortalFut é um agregador de notícias com uma seção atualizada sobre o Mundial, incluindo agenda dos jogos, datas, horários e detalhes de transmissões (TV e streaming). Em vez de navegar por vários sites, você encontra manchetes, análises e atualizações minuto a minuto em um só lugar. A plataforma faz a curadoria de conteúdo de fontes confiáveis, sem adicionar opiniões ou editar matérias originais.
+
+Redirecionamento para conteúdo original: clique nos links do [PortalFut](https://portalfut.com) para acessar as notícias.
+
+`;export{a as default};
