9241ff12e426820de9bfe91185887806cd49f325
everton-tenorio <everton-tenorio@users.noreply.github.com>
2025-06-30 20:17:53 +0000

    deploy: 1a4204923f29b4c619b1fb4b7a5e343dcb7f777e

---
+++ _nuxt/CeWnZYEp.js
@@ -0,0 +1,38 @@
+const e=`---
+title: "Vou atravessar o oceano Atlântico sozinho de remo"
+date: 2025-07-01
+slug: vou-atravessar-o-oceano-atlantico-sozinho-de-remo
+description: ""
+tags: ["barco IAT", "almyr klink"]
+bannerImage: "https://media-cdn.tripadvisor.com/media/photo-s/1a/e4/d1/35/replica-do-paraty-usado.jpg"
+---
+
+Às vezes, pensando em alguns momentos bem loucos, de como a gente sai vivo, e até meio tranquilo depois de tudo, histórias da vida. Hoje tô obrigatoriamente mais sossegado. Novos hábitos, novos rumos, muita coisa já bem programada na mente. 
+O que também é uma loucura, diga-se de passagem. Porque chega uma hora em que você começa a eliminar erros, acertos, coisas, pessoas... e segue.
+
+Tem um ditado que diz algo como: doido só é doido quando encontra outro e meio.
+
+Pois bem, meu Instagram, é todo configurado pra funcionar de um jeito próprio, mas o infeliz do algoritmo sempre dá um jeito de exibir na minha tela freiras dançantes, pastores esquisitos, 
+fofoca de cidade pequena, fim de namoro de subcelebridade, etc. E olha que eu só curto coisa de tecnologia, Flamengo, ideias boas, uma ou outra pessoa que vale a pena seguir. E não adianta.
+
+Até que um reels aleatório apareceu. Um pai falando sobre como criou a filha em meio à tal da Geração Z. O vídeo bateu. Lembrei que conhecia o sujeito de algum lugar, alguma coisa a ver com o mar. Fui pesquisar. Daí caiu no meu colo o livro Cem Dias Entre Céu e Mar.
+
+Amyr Klink. Imagine o cara acordou um dia e decidiu:
+
+> Vou atravessar o oceano Atlântico sozinho de remo.
+
+E foi. O primeiro brasileiro a remar da África(Namíbia) até Salvador(Brasil), sozinho, em 1984, sem gps, internet e iphone, em um remo feita por ele mesmo. 
+Cem dias em alto-mar, remando por algo em torno de 3700 milhas. Nenhuma equipe de apoio, somente sobreviver e chegar no Brasil. Imagine dormir, o corpo para, mas o barco não.
+
+Entrou pra história, mas não suficiente, Amyr também foi pra Antártica mais de 20 vezes, construiu um veleiro polar, o Paratii, projetado pra resistir ao gelo, navegou por áreas que vários navios não se arriscariam. Ficou meses sozinho em pleno oceano, enfrentou temperaturas absurdas, solidão, silêncio, etc.
+
+Precisa ser muito doido pra fazer isso, sair da zona de conforto assim, bater de frente com o nada, abrir mão do “certo”, da estabilidade, do dinheiro, de contrariar, e simplesmente dizer: foda-se. Dá meu barco e já era.
+
+A história do Amyr apareceu num momento certo pra mim. Não é apreço pela falta de noção, o diferentão. Mas porque ali tem algo real. Propósito. Esforço. Simplicidade. E essa ideia de que você pode saber muito, mesmo sem diploma na parede, abandonar 
+o supérfluo, superficial, o caminho mais fácil, likes, elimiar o desnecessário, encontrar novas respostas na natureza, não desperdiçar, sobre o que realmente é necessário para viver, o que importa e vale, mesmo que muitas vezes completamente sozinho.
+
+As aventuras do Amyr estão documentadas. Tem livro, entrevista, documentário. Procura na Amazon ou no YouTube. Enquanto isso, sigo vivendo minhas loucuras não documentadas, e quem sabe, de forma despretensiosa, algum algoritmo ou só o acaso mesmo, me indique aqueles que também anda na contramão, que prefere ir até a Antártica conversar com penguins ou focas. 
+
+---
+
+Amyr Klink é empresário, palestrante, navegador e escritor. Casado com Marina Klink, tem três filhas, uma delas, Tamara Klink, também se tornou navegadora e já fez suas próprias travessias. Além disso, ele é sócio fundador do Museu Nacional do Mar e está envolvido com a construção e gestão de marinas, como a Marina do Engenho em Paraty.`;export{e as default};
