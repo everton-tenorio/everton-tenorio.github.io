f1a865a04949b294142a5718be978e9bf35d4362
everton-tenorio <everton-tenorio@users.noreply.github.com>
2025-10-21 16:17:42 +0000

    deploy: ea3e9a73873ef99be9d8613e8b37553e9822f446

---
+++ _nuxt/t4UIKgM5.js
@@ -0,0 +1,30 @@
+const a=`---
+title: "calisthenic-ai: vibe codando uma aplicação para treinar sozinho"
+date: 2025-10-22
+slug: calisthenic-ai-vibe-codando-uma-aplicacao-para-treinar-sozinho
+description: ""
+tags: ["calistenia", "tecnologia"]
+bannerImage: "https://i.ibb.co/d45MDyyg/calisthenicai.png"
+---------------
+Lá pelos meus 16 anos, comecei a "treinar sozinho", era o termo que eu usava para fazer exercícios físicos que depois de um bom tempo, descobri que se chamavam **calistenia**.
+É o tipo de atividade que mais curto, e pratiquei de forma constante até mais ou menos os 22 anos.
+
+De uns tempos pra cá, o **código**, o **trabalho** e outras demandas acabaram me afastando dessa rotina. O treino virou algo de "tempos em tempos", sem muita constância.
+
+Em 2024, decidi resolver isso criando uma **aplicação para me ajudar a contabilizar os exercícios**. A ideia era simples: registrar séries, repetições, tempo e depois analisar tudo em gráficos, dashboards, etc. A intenção era melhorar a performance em cada exercício: flexões, barras, paralelas e por aí vai.
+
+O resultado? 
+
+> Nem terminei o app... e nem mudei o shape 🤣.
+
+Mesmo assim, continuei treinando de vez em quando. Mas, aproveitei a ideia anterior e criei uma nova versão, agora com uma novidade: **contabilizar as repetições por áudio e registrar automaticamente na aplicação**.
+
+No pain no gain, ou seja, o **Claude** (modelo da Anthropic) até tentou me ajudar com essa parte, mas não conseguiu entregar a feature com sucesso. Penso em mudar muita coisa de css, mas ainda assim, a aplicação ficou **usável** e para mim está ok, tendo resultados é o que importa.
+
+<center><img src="https://i.ibb.co/Vpp2jnnj/Screenshot-2025-10-21-at-12-31-41-Calisthenics-Pro-Performance-Tracker.png" width=60% /></center>
+
+Você pode acessar aqui: [**Calisthenic-ai**](https://calisthenic-ai.vercel.app/).
+
+Se você é dev, fique à vontade para **explorar o código** que está tudo no html, sugerir melhorias ou até criar seu próprio app com base nele.
+E, claro... **aproveite pra sair um pouco da frente do computador, treinar pow.**
+`;export{a as default};
