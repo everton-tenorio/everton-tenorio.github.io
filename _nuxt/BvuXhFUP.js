935fcefbcd93c3e9a392517e7b98c0134ad8ba6b
everton-tenorio <everton-tenorio@users.noreply.github.com>
2025-10-21 16:56:12 +0000

    deploy: 8494b5463628ae7236fb0ef8dbfa47bd61019da7

--- _nuxt/BvuXhFUP.js
+++ _nuxt/BvuXhFUP.js
@@ -9,7 +9,7 @@ bannerImage: "https://i.ibb.co/d45MDyyg/calisthenicai.png"
 Lá pelos meus 16 anos, comecei a "treinar sozinho", era o termo que eu usava para fazer exercícios físicos que depois de um bom tempo, descobri que se chamavam **calistenia**.
 É o tipo de atividade que mais curto, e pratiquei de forma constante até mais ou menos os 22 anos.
 
-De uns tempos pra cá, o **código**, o **trabalho** e outras demandas acabaram me afastando dessa rotina. O treino virou algo de "tempos em tempos", sem muita constância.
+De uns tempos pra cá, o código, o trabalho e outras demandas acabaram me afastando dessa rotina. O treino virou algo de "tempos em tempos", sem muita constância.
 
 Em 2024, decidi resolver isso criando uma **aplicação para me ajudar a contabilizar os exercícios**. A ideia era simples: registrar séries, repetições, tempo e depois analisar tudo em gráficos, dashboards, etc. A intenção era melhorar a performance em cada exercício: flexões, barras, paralelas e por aí vai.
 
@@ -19,12 +19,12 @@ O resultado?
 
 Mesmo assim, continuei treinando de vez em quando. Mas, aproveitei a ideia anterior e criei uma nova versão, agora com uma novidade: **contabilizar as repetições por áudio e registrar automaticamente na aplicação**.
 
-No pain no gain, ou seja, o **Claude** (modelo da Anthropic) até tentou me ajudar com essa parte, mas não conseguiu entregar a feature com sucesso. Penso em mudar muita coisa de css, mas ainda assim, a aplicação ficou **usável** e para mim está ok, tendo resultados é o que importa.
+No pain no gain, ou seja, o **Claude** (modelo da Anthropic) até tentou me ajudar com essa parte, mas não conseguiu entregar a feature com sucesso. Penso em mudar muita coisa de css, mas ainda assim, a aplicação ficou usável e para mim está ok, tendo resultados é o que importa.
 
 <center><img src="https://i.ibb.co/Vpp2jnnj/Screenshot-2025-10-21-at-12-31-41-Calisthenics-Pro-Performance-Tracker.png" width=60% /></center>
 
 Você pode acessar aqui: [**Calisthenic-ai**](https://calisthenic-ai.vercel.app/).
 
-Se você é dev, fique à vontade para **explorar o código** que está tudo no html, sugerir melhorias ou até criar seu próprio app com base nele.
+Se você é dev, fique à vontade para explorar o código, sugerir melhorias ou até criar seu próprio app com base nele.
 E, claro... **aproveite pra sair um pouco da frente do computador, treinar pow.**
 `;export{a as default};
